c01a94acfdfc9bfc832149d14a46d5868ea99b16
buzzind99 <buzzind99@gmail.com>
2026-03-25 14:29:40 +0700

    codeforces_282A

---
+++ codeforces_282A.go
@@ -0,0 +1,44 @@
+//go:build codeforces_282A
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func next() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	i, _ := strconv.Atoi(next())
+	return i
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	sol := 0
+    
+	n := nextInt()
+	for range n {
+		str := next()
+		if str[1] == '+' {
+			sol++
+		} else {
+			sol--
+		}
+	}
+
+	fmt.Println(sol)
+}
+
+/*
+  Link: https://codeforces.com/problemset/problem/282/A
+  Tags: implementation
+*/
