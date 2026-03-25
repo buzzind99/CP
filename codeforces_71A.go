51d5bd79f40617acf5c3e956d73e308b4e65501c
buzzind99 <buzzind99@gmail.com>
2026-03-25 13:43:32 +0700

    codeforces_71A

---
+++ codeforces_71A.go
@@ -0,0 +1,47 @@
+//go:build codeforces_71A
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func next() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	i, _ := strconv.Atoi(next())
+	return i
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+    
+	t := nextInt()
+	for range t {
+		solve()
+	}
+}
+
+func solve() {
+	str := next()
+	n := len(str)
+
+    if n > 10 {
+        fmt.Printf("%c%d%c\n", str[0], n-2, str[n-1])
+    } else {
+        fmt.Println(str)
+    }
+}
+
+/*
+  Link: https://codeforces.com/problemset/problem/71/A
+  Tags: strings
+*/
