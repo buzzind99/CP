3ae6e7b3a6b9cb95a798b6fb93e470cbcbefc9a3
buzzind99 <buzzind99@gmail.com>
2026-03-25 14:11:30 +0700

    codeforces_231A

---
+++ codeforces_231A.go
@@ -0,0 +1,50 @@
+//go:build codeforces_231A
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func next() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	i, _ := strconv.Atoi(next())
+	return i
+}
+
+func main() {
+	sol := 0
+	sc.Split(bufio.ScanWords)
+    
+	n := nextInt()
+	for range n {
+		sure := count()
+		if sure >= 2 {
+			sol++
+		}
+	}
+
+	fmt.Println(sol)
+}
+
+func count() int {
+	petya := nextInt()
+	vasya := nextInt()
+	tonya := nextInt()
+
+	return petya + vasya + tonya
+}
+
+/*
+  Link: https://codeforces.com/problemset/problem/231/A
+  Tags: brute force, greedy
+*/
