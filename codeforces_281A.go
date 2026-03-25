106f643950bde18609f4365d64f8ff30260e9d0f
buzzind99 <buzzind99@gmail.com>
2026-03-25 22:19:58 +0700

    codeforces_281A

---
+++ codeforces_281A.go
@@ -0,0 +1,31 @@
+//go:build codeforces_281A
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func next() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+    
+	str := next()
+
+	fmt.Printf("%s%s\n", strings.ToUpper(string(str[0])), str[1:])
+}
+
+/*
+  Link: https://codeforces.com/problemset/problem/281/A
+  Tags: implementation
+  Rating: 800
+*/
