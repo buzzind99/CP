b97453b0ed195662f9c35c651e6e74d25d79b146
buzzind99 <buzzind99@gmail.com>
2026-03-26 22:30:50 +0700

    simplify

--- codeforces_996A.go
+++ codeforces_996A.go
@@ -25,29 +25,12 @@ func main() {
 	sc.Split(bufio.ScanWords)
 
     n := nextInt()
-	count := 0
-	for n > 0 {
-		if n >= 100 {
-			count += n/100
-			n %= 100
-		}
-		if n >= 20 {
-			count += n/20
-			n %= 20
-		}
-		if n >= 10 {
-			count += n/10
-			n %= 10
-		}
-		if n >= 5 {
-			count += n/5
-			n %= 5
-		}
-		if n >= 1 {
-			count += n
-			break
-		}
-	}
+	bills := []int{100, 20, 10, 5, 1}
+    count := 0
+    for _, bill := range bills {
+        count += n / bill
+        n %= bill
+    }
 
 	fmt.Println(count)
 }
