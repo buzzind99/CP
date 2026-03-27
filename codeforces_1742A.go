941f50f081306956abe5e0174d0f31d25e39372c
buzzind99 <buzzind99@gmail.com>
2026-03-27 15:24:26 +0700

    codeforces_1742A

---
+++ codeforces_1742A.go
@@ -0,0 +1,42 @@
+//go:build codeforces_1742A
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func next() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	i, _ := strconv.Atoi(next())
+	return i
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+
+	t := nextInt()
+    for range t {
+        a, b, c := nextInt(), nextInt(), nextInt()
+        if a == b+c || b == a+c || c == a+b {
+            fmt.Println("YES")
+        } else {
+            fmt.Println("NO")
+        }
+    }
+}
+
+/*
+  Link: https://codeforces.com/problemset/problem/1742/A
+  Tags: implementation
+  Rating: 800
+*/
