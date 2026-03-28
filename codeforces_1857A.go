ba3199f1d077d6f4999d7b7cbe23b725b5be5982
buzzind99 <buzzind99@gmail.com>
2026-03-28 20:43:40 +0700

    codeforces_1857A

---
+++ codeforces_1857A.go
@@ -0,0 +1,47 @@
+//go:build codeforces_1857A
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func next() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	i, _ := strconv.Atoi(next())
+	return i
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+
+	t := nextInt()
+    for range t {
+        n := nextInt()
+        sum := 0
+        for range n {
+            a := nextInt()
+            sum += a
+        }
+        if sum%2 == 0 {
+            fmt.Println("YES")
+        } else {
+            fmt.Println("NO")
+        }
+    }
+}
+
+/*
+  Link: https://codeforces.com/problemset/problem/1857/A
+  Tags: greedy, math
+  Rating: 800
+*/
