cd3a5a59fc7857323e1ca5c5a6462532a769b96b
buzzind99 <buzzind99@gmail.com>
2026-03-29 00:24:54 +0700

    codeforces_1873A

---
+++ codeforces_1873A.go
@@ -0,0 +1,42 @@
+//go:build codeforces_1873A
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func next() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	i, _ := strconv.Atoi(next())
+	return i
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+
+	t := nextInt()
+    for range t {
+        s := next()
+        if s[0] == 'a' || s[1] == 'b' || s[2] == 'c' {
+            fmt.Println("YES")
+        } else {
+            fmt.Println("NO")
+        }
+    }
+}
+
+/*
+  Link: https://codeforces.com/problemset/problem/1873/A
+  Tags: brute force, implementation
+  Rating: 800
+*/
