9035913cd4b5281740c7fd8da7bc15e48bc9e97c
buzzind99 <buzzind99@gmail.com>
2026-03-29 16:05:31 +0700

    simplify

--- codeforces_490A.go
+++ codeforces_490A.go
@@ -25,29 +25,17 @@ func main() {
 	sc.Split(bufio.ScanWords)
 
 	n := nextInt()
-	arr := make([]int, n)
-	a, b, c := 0, 0, 0
-	for i := range n {
-		n := nextInt()
-		if n == 1 { a++ } else if n == 2 { b++ } else { c++ }
-		arr[i] = n
-	}
+	var arr [4][]int 
+    for i := 1; i <= n; i++ {
+        skill := nextInt()
+        arr[skill] = append(arr[skill], i)
+    }
 
-	minC := min(a, b, c)
-	fmt.Println(minC)
-	for range minC {
-		curr := 1
-		for i := 0; i < len(arr); i++ {
-			if curr > 3 { break }
-			if arr[i] == curr {
-				fmt.Print(i+1, " ")
-				arr[i] = 0
-				i = -1
-				curr++
-			}
-		}
-		fmt.Println()
-	}
+    numTeams := min(len(arr[1]), len(arr[2]), len(arr[3]))
+    fmt.Println(numTeams)
+    for i := range numTeams {
+        fmt.Printf("%d %d %d\n", arr[1][i], arr[2][i], arr[3][i])
+    }
 }
 
 /*
