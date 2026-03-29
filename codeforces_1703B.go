8c57df3f4ce009712f66c91481c188933048bd23
buzzind99 <buzzind99@gmail.com>
2026-03-29 18:21:23 +0700

    codeforces_1703B

---
+++ codeforces_1703B.go
@@ -0,0 +1,49 @@
+//go:build codeforces_1703B
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func next() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	i, _ := strconv.Atoi(next())
+	return i
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+
+	t := nextInt()
+    for range t {
+        m := make(map[byte]bool)
+        n := nextInt()
+        s := next()
+        sum := 0
+        for i := range n {
+            if !m[s[i]] {
+                m[s[i]] = true
+                sum += 2
+            } else {
+                sum++
+            }
+        }
+        fmt.Println(sum)
+    }
+}
+
+/*
+  Link: https://codeforces.com/problemset/problem/1703/B
+  Tags: data structures, implementation
+  Rating: 800
+*/
