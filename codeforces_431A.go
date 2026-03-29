f27b040d7e6f28530aedd66ca03369b38709e4d0
buzzind99 <buzzind99@gmail.com>
2026-03-29 18:09:17 +0700

    codeforces_431A

---
+++ codeforces_431A.go
@@ -0,0 +1,42 @@
+//go:build codeforces_431A
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func next() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	i, _ := strconv.Atoi(next())
+	return i
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+
+	arr := [5]int{0, nextInt(), nextInt(), nextInt(), nextInt()}
+	s := next()
+	sum := 0
+	for _, v := range s {
+		n := int(v-'0') 
+		sum += arr[n]
+	}
+
+    fmt.Println(sum)
+}
+
+/*
+  Link: https://codeforces.com/problemset/problem/431/A
+  Tags: implementation
+  Rating: 800
+*/
