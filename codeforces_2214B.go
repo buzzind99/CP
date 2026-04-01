cf215fea66034446656d3036196854d32923b231
buzzind99 <buzzind99@gmail.com>
2026-04-01 22:42:20 +0700

    codeforces_2214B

---
+++ codeforces_2214B.go
@@ -0,0 +1,37 @@
+//go:build codeforces_2214B
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func next() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	i, _ := strconv.Atoi(next())
+	return i
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	defer wr.Flush()
+
+	next()
+	fmt.Fprintln(wr, "1F604")
+}
+
+/*
+  Link: https://codeforces.com/problemset/problem/2214/B
+  Tags: april fools
+  Rating: -
+*/
