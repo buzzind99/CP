a19bde86b4ae670e1cb41dff153865de9169bd8c
buzzind99 <buzzind99@gmail.com>
2026-04-05 19:56:00 +0700

    codeforces_1807B

---
+++ codeforces_1807B.go
@@ -0,0 +1,50 @@
+//go:build codeforces_1807B
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func next() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	i, _ := strconv.Atoi(next())
+	return i
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	defer wr.Flush()
+
+	t := nextInt()
+	for range t {
+		n := nextInt()
+		mihai, bianca := 0, 0
+		for range n {
+			a := nextInt()
+			if a%2 == 0 { mihai += a } else { bianca += a }
+		}
+
+		if mihai > bianca {
+			fmt.Fprintln(wr, "YES")
+		} else {
+			fmt.Fprintln(wr, "NO")
+		}
+	}
+}
+
+/*
+  Link: https://codeforces.com/problemset/problem/1807/B
+  Tags: greedy
+  Rating: 800
+*/
