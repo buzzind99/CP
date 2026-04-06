4cd0a78abc0104a5cdff4b7caeb2caf304bc9072
buzzind99 <buzzind99@gmail.com>
2026-04-06 16:21:38 +0700

    simplify

--- codeforces_1950C.go
+++ codeforces_1950C.go
@@ -32,19 +32,12 @@ func main() {
 		hour, _ := strconv.Atoi(s[:2])
 		minute, _ := strconv.Atoi(s[3:])
 
-		if hour >= 12 {
-			if hour == 12 {
-				fmt.Fprintf(wr, "%02d:%02d PM\n", 12, minute)
-			} else {
-				fmt.Fprintf(wr, "%02d:%02d PM\n", hour%12, minute)
-			}
-		} else {
-			if hour == 0 {
-				fmt.Fprintf(wr, "%02d:%02d AM\n", 12, minute)
-			} else {
-				fmt.Fprintf(wr, "%02d:%02d AM\n", hour%12, minute)
-			}
-		}
+		h := hour%12
+		period := "AM"
+		if h == 0 { h = 12 }
+		if hour >= 12 { period = "PM" }
+
+		fmt.Fprintf(wr, "%02d:%02d %s\n", h, minute, period)
 	}
 }
 
