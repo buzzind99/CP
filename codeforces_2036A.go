56f166d5eb69ffb8bcffcbfd1bdfa5c234863ca8
buzzind99 <buzzind99@gmail.com>
2026-04-21 08:24:05 +0700

    codeforces_2036A

---
+++ codeforces_2036A.go
@@ -0,0 +1,58 @@
+//go:build codeforces_2036A
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func next() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	i, _ := strconv.Atoi(next())
+	return i
+}
+
+func abs(x int) int {
+	if x < 0 { return -x }
+	return x
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	defer wr.Flush()
+
+	t := nextInt()
+	for range t {
+		n := nextInt()
+		last := nextInt()
+		perfect := true
+		for range n-1 {
+			curr := nextInt()
+			r := abs(last-curr)
+			if r != 5 && r != 7 { perfect = false }
+			last = curr
+		}
+
+		if perfect {
+			fmt.Fprintln(wr, "YES")
+		} else {
+			fmt.Fprintln(wr, "NO")
+		}
+	}
+}
+
+/*
+  Link: https://codeforces.com/problemset/problem/2036/A
+  Tags: implementation
+  Rating: 800
+*/
