e6492f85d79bb9449fcc639656dc49f57dac169b
buzzind99 <buzzind99@gmail.com>
2026-04-27 11:50:07 +0700

    codeforces_1980B

---
+++ codeforces_1980B.go
@@ -0,0 +1,59 @@
+//go:build codeforces_1980B
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"slices"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func next() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	i, _ := strconv.Atoi(next())
+	return i
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	defer wr.Flush()
+
+	t := nextInt()
+	for range t {
+		n, f, k := nextInt(), nextInt(), nextInt()
+		arr := make([]int, 0, n)
+		for range n {
+			arr = append(arr, nextInt())
+		}
+		fav := arr[f-1]
+		slices.Sort(arr)
+		slices.Reverse(arr)
+
+		if arr[k-1] > fav {
+			fmt.Fprintln(wr, "NO")
+		} else if arr[k-1] < fav {
+			fmt.Fprintln(wr, "YES")
+		} else {
+			if k < n && arr[k] == fav {
+				fmt.Fprintln(wr, "MAYBE")
+			} else {
+				fmt.Fprintln(wr, "YES")
+			}
+		}
+	}
+}
+
+/*
+  Link: https://codeforces.com/problemset/problem/1980/B
+  Tags: implementation
+  Rating: 800
+*/
