fe634673888aeeea96d739b3ba47fb2163731b56
buzzind99 <buzzind99@gmail.com>
2026-04-30 11:08:03 +0700

    codeforces_1650A

---
+++ codeforces_1650A.go
@@ -0,0 +1,53 @@
+//go:build codeforces_1650A
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func next() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	i, _ := strconv.Atoi(next())
+	return i
+}
+
+func isEven (n int) bool {
+	return n%2 == 0
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	defer wr.Flush()
+
+	t := nextInt()
+	for range t {
+		s, c := next(), next()
+		found := false
+		for i := range s {
+			if s[i] == c[0] && isEven(i) { found = true }
+		}
+
+		if found {
+			fmt.Fprintln(wr, "YES")
+		} else {
+			fmt.Fprintln(wr, "NO")
+		}
+	}
+}
+
+/*
+  Link: https://codeforces.com/problemset/problem/1650/A
+  Tags: implementation, strings
+  Rating: 800
+*/
