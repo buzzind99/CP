4ab3406ebe0d6ba7af887c7d9b8f294128670190
buzzind99 <buzzind99@gmail.com>
2026-05-05 16:21:58 +0700

    codeforces_1690A

---
+++ codeforces_1690A.go
@@ -0,0 +1,48 @@
+//go:build codeforces_1690A
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func next() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	i, _ := strconv.Atoi(next())
+	return i
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	defer wr.Flush()
+
+	t := nextInt()
+	for range t {
+		n := nextInt()
+		nMod := n%3
+
+		if nMod == 0 {
+			fmt.Fprintln(wr, n/3, n/3+1, n/3-1)
+		} else if nMod == 1 {
+			fmt.Fprintln(wr, n/3, n/3+2, n/3-1)
+		} else {
+			fmt.Fprintln(wr, n/3+1, n/3+2, n/3-1)
+		}
+	}
+}
+
+/*
+  Link: https://codeforces.com/problemset/problem/1690/A
+  Tags: constructive algorithms, greedy
+  Rating: 800
+*/
