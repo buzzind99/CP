91d5ac58f55e853b9e6b849d9fc694fe0d4202b3
buzzind99 <buzzind99@gmail.com>
2026-05-06 17:37:59 +0700

    codeforces_1996B

---
+++ codeforces_1996B.go
@@ -0,0 +1,50 @@
+//go:build codeforces_1996B
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func next() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	i, _ := strconv.Atoi(next())
+	return i
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	defer wr.Flush()
+
+	t := nextInt()
+	for range t {
+		n, k := nextInt(), nextInt()
+		count := 0
+		for range n {
+			s := next()
+			if count%k == 0 {
+				for i := 0; i < n; i += k {
+					fmt.Fprint(wr, string(s[i]))
+				}
+				fmt.Fprintln(wr)
+			}
+			count++
+		}
+	}
+}
+
+/*
+  Link: https://codeforces.com/problemset/problem/1996/B
+  Tags: greedy, implementation
+  Rating: 800
+*/
