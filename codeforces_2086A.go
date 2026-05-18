12f96ddd4cabaab84c41f1c4d2c3288599ff0e2b
buzzind99 <buzzind99@gmail.com>
2026-05-18 14:26:51 +0700

    codeforces_2086A

---
+++ codeforces_2086A.go
@@ -0,0 +1,41 @@
+//go:build codeforces_2086A
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func next() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	i, _ := strconv.Atoi(next())
+	return i
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	defer wr.Flush()
+
+	t := nextInt()
+	for range t {
+		n := nextInt()
+
+		fmt.Fprintln(wr, n*2)
+	}
+}
+
+/*
+  Link: https://codeforces.com/problemset/problem/2086/A
+  Tags: math
+  Rating: 800
+*/
