21c3c6e766684c06dd10ab63375fdf66f102193c
buzzind99 <buzzind99@gmail.com>
2026-05-24 14:32:28 +0700

    codeforces_2133A

---
+++ codeforces_2133A.go
@@ -0,0 +1,52 @@
+//go:build codeforces_2133A
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func next() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	i, _ := strconv.Atoi(next())
+	return i
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	defer wr.Flush()
+
+	t := nextInt()
+	for range t {
+		n := nextInt()
+		seen := make(map[int]bool)
+		possible := false
+		for range n {
+			teeth := nextInt()
+			if seen[teeth] { possible = true }
+			seen[teeth] = true
+		}
+
+		if possible {
+			fmt.Fprintln(wr, "YES")
+		} else {
+			fmt.Fprintln(wr, "NO")
+		}
+	}
+}
+
+/*
+  Link: https://codeforces.com/problemset/problem/2133/A
+  Tags: brute force, data structures, implementation, math
+  Rating: 800
+*/
