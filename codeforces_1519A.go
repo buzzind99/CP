7e95dd68ff0e068696f282be7c81f4f66e053aa8
buzzind99 <buzzind99@gmail.com>
2026-05-28 13:11:19 +0700

    codeforces_1519A

---
+++ codeforces_1519A.go
@@ -0,0 +1,56 @@
+//go:build codeforces_1519A
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func next() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	i, _ := strconv.Atoi(next())
+	return i
+}
+
+func isEven(n int) bool {
+	return n%2 == 0
+}
+
+func abs(x int) int {
+	if x < 0 { return -x }
+	return x
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	defer wr.Flush()
+
+	t := nextInt()
+	for range t {
+		r, b, d := nextInt(), nextInt(), nextInt()
+		minColor := min(r, b)
+		maxColor := max(r, b)
+
+		if maxColor <= minColor*(1+d) {
+			fmt.Fprintln(wr, "YES")
+		} else {
+			fmt.Fprintln(wr, "NO")
+		}
+	}
+}
+
+/*
+  Link: https://codeforces.com/problemset/problem/1519/A
+  Tags: math
+  Rating: 800
+*/
