53472d11665bfc411bf05f4fcdcc0e4bf24ffb20
buzzind99 <buzzind99@gmail.com>
2026-06-01 19:25:04 +0700

    codeforces_2149B

---
+++ codeforces_2149B.go
@@ -0,0 +1,53 @@
+//go:build codeforces_2149B
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"slices"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func next() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	i, _ := strconv.Atoi(next())
+	return i
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	defer wr.Flush()
+
+	t := nextInt()
+	for range t {
+		n := nextInt()
+		arr := make([]int, 0, n)
+		for range n {
+			a := nextInt()
+			arr = append(arr, a)
+		}
+		slices.Sort(arr)
+
+		minDiff := -1
+		for i := 0; i < n-1; i += 2 {
+			minDiff = max(minDiff, arr[i+1]-arr[i])
+		}
+
+		fmt.Fprintln(wr, minDiff)
+	}
+}
+
+/*
+  Link: https://codeforces.com/problemset/problem/2149/B
+  Tags: greedy, sortings
+  Rating: 800
+*/
