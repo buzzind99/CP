16dba16f308866f1d69936adc6a6ec6fa2ace37f
buzzind99 <buzzind99@gmail.com>
2026-06-04 13:30:36 +0700

    increase buffer size

--- codeforces_1968B.go
+++ codeforces_1968B.go
@@ -22,24 +22,8 @@ func nextInt() int {
 	return i
 }
 
-func gcd(a int, b int) int {
-	for b != 0 { a, b = b, a%b }
-	return a
-}
-
-func isPrime(n int) bool {
-	if n <= 1 { return false }
-	for i := 2; i*i <= n; i++ {
-		if n%i == 0 { return false }
-	}
-	return true
-}
-
-func isEven(n int) bool {
-	return n%2 == 0
-}
-
 func main() {
+	sc.Buffer(make([]byte, 1<<20), 1<<20)
 	sc.Split(bufio.ScanWords)
 	defer wr.Flush()
 
